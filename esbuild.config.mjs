ae1ab2dde247bb4a255f0af82be8a1560e2b4c00
Eric Oswald <zyon.900@googlemail.com>
2026-03-19 02:31:16 +0100

    setup preact

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -10,6 +10,7 @@ if you want to view the source, please visit the github repository of this plugi
 `;
 
 const prod = (process.argv[2] === "production");
+const nodeBuiltins = [...builtinModules, ...builtinModules.map((name) => `node:${name}`)];
 
 const context = await esbuild.context({
 	banner: {
@@ -31,9 +32,11 @@ const context = await esbuild.context({
 		"@lezer/common",
 		"@lezer/highlight",
 		"@lezer/lr",
-		...builtinModules],
+		...nodeBuiltins],
 	format: "cjs",
 	target: "es2018",
+	jsx: "automatic",
+	jsxImportSource: "preact",
 	logLevel: "info",
 	sourcemap: prod ? false : "inline",
 	treeShaking: true,
--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,62 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Notice, Plugin } from "obsidian";
+import { EncounterServer } from "./network/encounter-server";
+import { PreactMount } from "./ui/preact-mount";
+import { CleanupRegistry } from "./utils/cleanup-registry";
 
-// Remember to rename these classes and interfaces!
+export default class EncounterCastPlugin extends Plugin {
+	private readonly cleanupRegistry = new CleanupRegistry();
+	private readonly encounterServer = new EncounterServer();
+	private preactMount: PreactMount | null = null;
+	private statusBarRoot: HTMLElement | null = null;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	async onload(): Promise<void> {
+		this.statusBarRoot = this.addStatusBarItem();
+		this.statusBarRoot.addClass("encounter-cast-status-root");
+		this.preactMount = new PreactMount(this.statusBarRoot);
+		this.renderFoundationView();
 
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "start-local-server",
+			name: "Start local server",
+			callback: async () => {
+				const state = await this.encounterServer.start();
+				this.renderFoundationView();
+				new Notice(`Server started on port ${state.port ?? "unknown"}.`);
+			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
 		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
+			id: "stop-local-server",
+			name: "Stop local server",
+			callback: async () => {
+				await this.encounterServer.stop();
+				this.renderFoundationView();
+				new Notice("Server stopped.");
+			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
+		const refreshOnResize = () => {
+			this.cleanupRegistry.debounce("status-refresh", 120, () => this.renderFoundationView());
+		};
+		window.addEventListener("resize", refreshOnResize);
+		this.cleanupRegistry.add(() => {
+			window.removeEventListener("resize", refreshOnResize);
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
 	}
 
-	onunload() {
+	onunload(): void {
+		void this.encounterServer.stop();
+		this.preactMount?.unmount();
+		this.preactMount = null;
+		this.statusBarRoot = null;
+		this.cleanupRegistry.dispose();
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	private renderFoundationView(): void {
+		const state = this.encounterServer.getState();
+		this.preactMount?.update({
+			serverRunning: state.running,
+			serverPort: state.port,
+		});
 	}
 }
